e92294189b28d069746473a7963ddc98e004c649
Ruben Villalvazo <vim.ruben@hotmail.com>
2023-04-02 12:13:32 -0300

    Add data types

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 use std::io;
 
 fn fahrenheit_to_celsius(value: f64) -> f64 {
-    let celsius = (value - 32.0) / 1.8;
+    let celsius: f64 = (value - 32.0) / 1.8;
     return celsius;
 }
 
 fn celsius_to_fahrenheit(value: f64) -> f64 {
-    let fahrenheit = (value * 1.8) + 32.0;
+    let fahrenheit: f64 = (value * 1.8) + 32.0;
     return fahrenheit;
 }
 
 fn user_input() -> String {
-    let mut user_input = String::new();
+    let mut user_input: String = String::new();
     io::stdin()
         .read_line(&mut user_input)
         .expect("Failed to read line");
@@ -22,7 +22,7 @@ fn select_temperature() {
     'chose: loop {
         println!("Chose (F) for fahrenheit to celsius or (C) for celsius to fahrenheit: ");
 
-        let user_choice = user_input().trim().to_lowercase();
+        let user_choice: String = user_input().trim().to_lowercase();
 
         if user_choice == "f" {
             println!("Input the Fahrenheit value: ");
@@ -40,7 +40,7 @@ fn select_temperature() {
         } else if user_choice == "c" {
             println!("Input the Celsius value: ");
 
-            let celsius = user_input()
+            let celsius: f64 = user_input()
                 .trim()
                 .parse()
                 .expect("Must to input a number: found _string_ or _None_");
@@ -61,7 +61,7 @@ fn converter() {
     select_temperature();
     'converter: loop {
         println!("Would you like to do another convertion? (Y/N)");
-        let user_answer = user_input().trim().to_lowercase();
+        let user_answer: String = user_input().trim().to_lowercase();
         if user_answer == "y" || user_answer == "yes" {
             converter();
             break 'converter;
